0b83ce7ef7b04cdcdc044334284518cbba578be2
AndrewBoyne <150052807+AndrewBoyne@users.noreply.github.com>
2024-03-27 17:06:35 +0000

    after ratings and district ranking
    after ratings and district ranking

--- Sprint 2/After scoring ratings and ranking districts.sql
+++ Sprint 2/After scoring ratings and ranking districts.sql
@@ -4,4 +4,12 @@ from Reviews where Rating = 1
 group by district_id
 order by rating_count desc;
 
---select count(distinct district_id as
+select count(distinct district_id) as distinct_district_count
+from Reviews
+
+select district_id, COUNT(*) AS rating_count
+from Reviews where Rating = 1
+group by district_id
+order by rating_count desc
+--LIMIT TOP 20 PERCENT;
+--LIMIT (SELECT COUNT(DISTINCT district_id) * 0.2 from Reviews;
